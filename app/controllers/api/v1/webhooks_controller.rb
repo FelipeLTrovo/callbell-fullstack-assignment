0b60d473a7edec3e545c88ea22cac108d04a01b3
FelipeLTrovo <felipeltrovo@hotmail.com>
2024-03-17 12:18:49 -0300

    Webhooks controller changes.

--- app/controllers/api/v1/webhooks_controller.rb
+++ app/controllers/api/v1/webhooks_controller.rb
@@ -1,8 +1,42 @@
 class Api::V1::WebhooksController < ApplicationController
   def index
-    # TODO:
-    #   - insert the card to database if the event type is "createCard"
-    #   - update the card in database if the event type is "updateCard"
-    #   - delete the card in database if the event type is "deleteCard"
+    render status: 200
+  end
+
+  def create
+    case event_type
+
+    when 'createCard'
+      CardGenerator.generate_card(card_payload).save
+            
+    when 'updateCard'
+      CardUpdater.new(card_payload).update_card
+
+    when 'deleteCard'
+      CardDestroyer.new(card_payload).destroy_card
+
+    when 'createList'
+      ListGenerator.generate_list(list_payload).save
+
+    when 'updateList'
+      ListUpdater.new(list_payload).update_list
+    end
+    render status: 200
+  end
+
+  private
+
+  def event_type
+    request.request_parameters['action']['type']
+  end
+
+  def card_payload
+    return request.request_parameters['action']['data']['card'].merge({list_id: list_payload['id']}.with_indifferent_access) if list_payload.present?
+
+    request.request_parameters['action']['data']['card']
+  end
+
+  def list_payload
+    request.request_parameters['action']['data']['list']
   end
 end
--- spec/controllers/api/v1/webhooks_controller_spec.rb
+++ spec/controllers/api/v1/webhooks_controller_spec.rb
@@ -1,8 +1,37 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::WebhooksController, type: :controller do
-  describe "GET /index" do
-    # TODO: Test your trello webhook logic here. 
-    pending "add some examples (or delete) #{__FILE__}"
+  before(:all) { Rails.application.load_seed }
+
+  describe "POST /create" do
+    it 'returns a createCard event' do
+      expect(CardGenerator).to receive(:generate_card).with({"id"=>"65f4a9fa372ef7710e2f1204", "name"=>"Card name created by webhooks_spec"})
+      expect { post :create, params: { action: {type: 'createCard', data: {card: {"id"=>"65f4a9fa372ef7710e2f1204", "name"=>"Card name created by webhooks_spec"}}} }}.to raise_error(NoMethodError, "undefined method `save' for nil:NilClass")
+      expect(response).to be_successful
+    end
+
+    it 'returns a updateCard event' do
+      expect(CardUpdater).to receive(:new).with({'id'=> Card.first.trello_id})
+      expect { post :create, params: { action: {type: 'updateCard', data: {card: {id: Card.first.trello_id}}} }}.to raise_error(NoMethodError, "undefined method `update_card' for nil:NilClass")
+      expect(response).to be_successful
+    end
+
+    it 'returns a updateCard event' do
+      expect(CardDestroyer).to receive(:new).with({'id'=> Card.first.trello_id})
+      expect { post :create, params: { action: {type: 'deleteCard', data: {card: {id: Card.first.trello_id}}} }}.to raise_error(NoMethodError, "undefined method `destroy_card' for nil:NilClass")
+      expect(response).to be_successful
+    end
+
+    it 'returns a createList event' do
+      expect(ListGenerator).to receive(:generate_list).with({"id"=>"65f4a9fa372ef7710e2f1204", "name"=>"List name created by webhooks_spec"})
+      expect { post :create, params: { action: {type: 'createList', data: {list: {"id"=>"65f4a9fa372ef7710e2f1204", "name"=>"List name created by webhooks_spec"}}} }}.to raise_error(NoMethodError, "undefined method `save' for nil:NilClass")
+      expect(response).to be_successful
+    end
+
+    it 'returns a updateList event' do
+      expect(ListUpdater).to receive(:new).with({'id'=> List.first.trello_id})
+      expect { post :create, params: { action: {type: 'updateList', data: {list: {id: List.first.trello_id}}} }}.to raise_error(NoMethodError, "undefined method `update_list' for nil:NilClass")
+      expect(response).to be_successful
+    end
   end
 end
