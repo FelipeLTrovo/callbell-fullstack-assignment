51d13467d380e155c4bd48a15c794b9270431f94
FelipeLTrovo <felipeltrovo@hotmail.com>
2024-03-17 12:15:13 -0300

    Adds db:seeds.

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,12 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+trello_client = TrelloWrapper.new
+
+trello_client.lists.each do |list|
+  db_list = List.find_or_create_by(name: list.name, trello_id: list.id)
+
+  trello_client.cards(list).each do |card|
+    Card.find_or_create_by(name: card.name, trello_id: card.id, list: db_list, description: card.desc)
+  end
+end
