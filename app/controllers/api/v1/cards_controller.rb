bb35883860a8c064d146842109c55e7eede558d9
FelipeLTrovo <felipeltrovo@hotmail.com>
2024-03-17 12:17:31 -0300

    Cards controller changes.

--- app/controllers/api/v1/cards_controller.rb
+++ app/controllers/api/v1/cards_controller.rb
@@ -1,11 +1,41 @@
 class Api::V1::CardsController < ApplicationController
-  # TODO: Fill the controller actions for the API
   def index
+    @cards = Card.all
+    render :json => @cards, status: 200
   end
 
   def create
-    # TODO:
-    #  - add body params validation
-    #  - push card to Trello
+    @card = CardGenerator.generate_card(card_params)
+    
+    if @card.save
+      @client = TrelloWrapper.new
+      res = @client.create_card(name: card_params['name'], description: card_params['desc'], list_id: card_params['list_id'])
+      @card.update(trello_id: res.id)
+      render :json => @card, status: 200
+    end
+  end
+
+  def update
+    card = CardUpdater.new(card_params).update_card
+    if card.valid?
+      client = TrelloWrapper.new
+      client.update_card(card.attributes)
+      render :json => card, status: 200
+    end
+  end
+
+  def destroy
+    card = CardDestroyer.new(card_params).destroy_card
+    if card.present?
+      client = TrelloWrapper.new
+      client.destroy_card(card.trello_id)
+      render :json => card, status: 200
+    end
+  end
+
+  private
+
+  def card_params
+    params.require(:card).permit(:id, :name, :trello_id, :list_id, :desc) if params.has_key?(:card)
   end
 end
--- spec/controllers/api/v1/cards_controller_spec.rb
+++ spec/controllers/api/v1/cards_controller_spec.rb
@@ -1,13 +1,53 @@
 require 'rails_helper'
 
 RSpec.describe Api::V1::CardsController, type: :controller do
+  before(:all) { Rails.application.load_seed }
+
   describe "GET /index" do
-    # TODO: Test your card API here
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns a successful response' do
+      get :index
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+    end
   end
 
   describe "POST /create" do
-    # TODO: Test your card API here
-    pending "add some examples (or delete) #{__FILE__}"
+    it 'returns a successful response' do
+      post :create, params: { card: {name: 'Card name created by cards_spec', list_id: List.first.trello_id}}
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns a unsuccessful response when invalid params' do
+      post :create, params: { card: {wrong_param: 'Wrong param'}}
+      expect(response).to be_successful
+      expect(response.status).not_to eq(200)
+    end
+  end
+
+  describe "PATCH /update" do
+    it 'returns a successful response' do
+      patch :update, params: { id: Card.last.id, card: {name: 'Card name created by cards_spec update', id: Card.last.id, trello_id: Card.last.trello_id}}
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns a unsuccessful response when invalid params' do
+      patch :update, params: {id: Card.first.id, card: {name: '', id: Card.first.id, trello_id: Card.first.trello_id}}
+      expect(response).to be_successful
+      expect(response.status).not_to eq(200)
+    end
+  end
+
+  describe "DELETE /destroy" do
+    it 'returns a successful response' do
+      delete :destroy, params: {id: Card.last.id, card: {id: Card.last.trello_id}}
+      expect(response).to be_successful
+      expect(response.status).to eq(200)
+    end
+
+    it 'returns an error when invalid params' do
+      expect {delete :destroy, params: { wrong_param: 'abc123' }}.to raise_error(ActionController::UrlGenerationError)
+    end
   end
 end
