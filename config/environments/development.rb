44dc238260ad7d161faac5af5df85a465199f8a8
FelipeLTrovo <felipeltrovo@hotmail.com>
2024-03-17 12:10:47 -0300

    Some development environment configuration.

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -8,6 +8,9 @@ Rails.application.configure do
   # since you don't have to restart the web server when you make code changes.
   config.cache_classes = false
 
+  # Adds ngrok to allowed hosts
+  config.hosts << /[a-z0-9\-]+\.ngrok-free\.app/
+  
   # Do not eager load code on boot.
   config.eager_load = false
 
