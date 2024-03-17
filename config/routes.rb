ae3b24a5d4efdd1893a4dbd79389c3017b6832e3
FelipeLTrovo <felipeltrovo@hotmail.com>
2024-03-17 12:12:54 -0300

    Adds routes for lists..

--- config/routes.rb
+++ config/routes.rb
@@ -1,8 +1,9 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
-      resource :webhooks
+      resources :webhooks
       resources :cards
+      resources :lists
     end
   end
 
